3e9ef2a48fa0d8fd55b56cef3ad93b2f19d16888
JoogonGo <JoogonGo@users.noreply.github.com>
2023-04-11 17:11:48 +0900

    10172번 upload

---
+++ 10172.cpp
@@ -0,0 +1,16 @@
+/*
+* 따옴표는 \붙이면 된다.
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "|\\_/|" << endl;
+	cout << "|q p|   /}" << endl;
+	cout << "( 0 )\"\"\"\\" << endl;
+	cout << "|\"^\"`    |" << endl;
+	cout << "||_/=\\\\__|" << endl;
+	return 0;
+}
