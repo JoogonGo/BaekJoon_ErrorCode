346db44182e60bb4c68d4dd94d8c4f32e67da38c
JoogonGo <JoogonGo@users.noreply.github.com>
2023-04-12 16:26:30 +0900

    10952번 upload

---
+++ 10952.cpp
@@ -0,0 +1,31 @@
+/*
+* 특이사항 없음..
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int nA, nB;
+	while(true)
+	{
+		cin >> nA >> nB;
+		if (nA < 0 || nA >= 10 || nB < 0 || nB >= 10)
+		{
+			return 0;
+		}
+		else if (nA == 0 && nB == 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << nA + nB << "\n";
+		}
+	}
+
+	return 0;
+}
